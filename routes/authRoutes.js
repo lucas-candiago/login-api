05db0a718e0a382222b11827f1a859654f62adc9
LucasCandiago <lucas.teixeira@maxiquim.com.br>
2025-03-28 11:12:24 -0300

    Add token validation

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,64 +1,67 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
 const User = require("../models/User")
+const verifyToken = require("../middlewares/authMiddleware"); // Import verifyToken middleware
 
 const router = express.Router()
 
-// Endpoint de registro
+// register endpoint
 router.post("/register", async (req, res) => {
     const { name, email, password } = req.body
 
     try {
         const userExists = await User.findOne({ email })
-        if (userExists) return res.status(400).json({ message: "Email já registrado" })
+        if (userExists) return res.status(400).json({ message: "Email already registered" })
 
-        const hashedPassword = await bcrypt.hash(password, 10)
+        const hashedPassword = await bcrypt.hash(password, 10)   // encrypting password before saving it to the database
         const newUser = new User({ name, email, password: hashedPassword })
 
-        await newUser.save()
-        res.status(201).json({ message: "Usuário criado com sucesso!" })
+        await newUser.save()  // saving the new user to the database
+        res.status(201).json({ message: "User created successfully" })
     } catch (error) {
-        res.status(500).json({ message: "Erro no server" })
+        res.status(500).json({ message: "Server error" })
     }
 })
 
+// login endpoint 
 router.post("/login", async (req, res) => {
     const { email, password } = req.body
 
     try {
         const user = await User.findOne({ email })
-        if (!user) return res.status(400).json({ message: "Usuário não encontrado" })
+        if (!user) return res.status(400).json({ message: "User not found" })
 
-        const isMatch = await bcrypt.compare(password, user.password)
-        if (!isMatch) return res.status(400).json({ message: "Senha incorreta!" })
+        const isMatch = await bcrypt.compare(password, user.password)  // verifying if the encrypted password matches the password in the database
+        if (!isMatch) return res.status(400).json({ message: "Password does not match" })
 
-        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: "1h" })
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: "1h" })  // setting jwt token for 1 hour (if you want more time, change in expiresIn)
 
         res.json({ token, user: { id: user._id, name: user.name, email: user.email } })
     }
     catch (error) {
-        res.status(500).json({ message: "Erro no server" })
+        res.status(500).json({ message: "Server error" })
     }
 })
 
-router.post("/change-password", async (req, res) => {
-    const { email, oldPassword, newPassword } = req.body
+
+// change password endpoint
+router.post("/change-password", verifyToken, async (req, res) => {
+    const { oldPassword, newPassword } = req.body
 
     try {
-        const user = await User.findOne({ email })
-        if (!user) return res.status(400).json({ message: "Usuário não encontrado" })
+        const user = await User.findById(req.userId)
+        if (!user) return res.status(400).json({ message: "User not found" })
 
-        const oldPasswordMatch = await bcrypt.compare(oldPassword, user.password)
-        if (!oldPasswordMatch) return res.status(400).json({ message: "Sua senha antiga está incorreta!" })
+        const oldPasswordMatch = await bcrypt.compare(oldPassword, user.password)  // verifying if old password is correct
+        if (!oldPasswordMatch) return res.status(400).json({ message: "Old password does not match" })
 
         const hashedNewPassword = await bcrypt.hash(newPassword, 10)
         user.password = hashedNewPassword
 
         await user.save()
-        res.status(200).json({ message: "Senha alterada com sucesso!" })
+        res.status(200).json({ message: "Password changed successfully" })
     } catch (error) {
-        res.status(500).json({ message: "Erro no server" })
+        res.status(500).json({ message: "Server error" })
     }
 })
 
--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-// Connect no MongoDB
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
 
-// Rotas
+// Routes
 app.use("/users", require("./routes/authRoutes"))
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+const PORT = 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
